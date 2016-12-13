4700707ec42c176dda275e827db5f0aff1b05296
John <wei.jiang@winsuntech.cn>
2016-12-13 16:39:35 +0800

    Update install-appifi.sh

--- wisnuc-215i/ubuntu_16_04_1_amd64_core/install-appifi.sh
+++ wisnuc-215i/ubuntu_16_04_1_amd64_core/install-appifi.sh
@@ -39,11 +39,11 @@ banner "In install-appifi.sh file"
 # define all pathnames
 #
 # version
-# nodejs: 6.9.1
+# nodejs: 6.9.2
 #
-node_download_path="https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-x64.tar.xz"
-node_package_name="node-v6.9.1-linux-x64.tar.xz"
-node_home_path="node-v6.9.1-linux-x64"
+node_download_path="https://nodejs.org/dist/v6.9.2/node-v6.9.2-linux-x64.tar.xz"
+node_package_name="node-v6.9.2-linux-x64.tar.xz"
+node_home_path="node-v6.9.2-linux-x64"
 
 # version
 # docker: 1.12.3
