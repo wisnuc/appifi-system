65c82116dad286691dd34b2eb961471badf8204a
matianfu <lewis.ma@winsuntech.cn>
2017-08-29 17:43:12 +0800

    create ubuntu 16.04.3 directory

---
+++ install-scripts/ubuntu-16-04-3-amd64/install-appifi.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+set -e
+
+DASH="------------------------------------------------------------"
+
+function banner {
+	echo ""
+	echo $DASH
+	echo "$1"
+	echo $DASH
+	echo ""
+}
+
+cat <<'EOF'
+
+
+
+
+
+
+ __      __ .___   _________ _______    ____ ___ _________  
+/  \    /  \|   | /   _____/ \      \  |    |   \\_   ___ \ 
+\   \/\/   /|   | \_____  \  /   |   \ |    |   //    \  \/ 
+ \        / |   | /        \/    |    \|    |  / \     \____
+  \__/\  /  |___|/_______  /\____|__  /|______/   \______  /
+       \/                \/         \/                   \/ 
+
+            Copyright (c) 2014-2018 wisnuc.com
+
+
+        
+EOF
+
+bash -c "systemctl stop appifi-bootstrap.service > /dev/null 2>&1; exit 0"
+bash -c "systemctl stop appifi-bootstrap-update.service > /dev/null 2>&1; exit 0"
+
+banner "add docker repo"
+apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+banner "setup nodejs repo"
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+
+banner "install packages"
+apt -y install nodejs docker-ce avahi-daemon avahi-utils btrfs-tools imagemagick ffmpeg samba udisks2
+
+banner "stop and disable samba service"
+systemctl stop smbd nmbd
+systemctl disable smbd nmbd
+
+banner "pull bootstrap files"
+mkdir -p /wisnuc/bootstrap
+wget -O /wisnuc/bootstrap/appifi-bootstrap-update.packed.js  https://raw.githubusercontent.com/wisnuc/appifi-bootstrap-update/release/appifi-bootstrap-update.packed.js
+wget -O /wisnuc/bootstrap/appifi-bootstrap.js.sha1 https://raw.githubusercontent.com/wisnuc/appifi-bootstrap/release/appifi-bootstrap.js.sha1
+
+banner "install and start appifi-bootstrap and appifi-bootstrap-update service"
+cat > /lib/systemd/system/appifi-bootstrap.service <<EOF
+[Unit]
+Description=Appifi Bootstrap Server
+After=network.target
+
+[Service]
+Type=idle
+ExecStartPre=/bin/cp /wisnuc/bootstrap/appifi-bootstrap.js.sha1 /wisnuc/bootstrap/appifi-bootstrap.js
+ExecStart=/usr/bin/node /wisnuc/bootstrap/appifi-bootstrap.js
+TimeoutStartSec=3
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /lib/systemd/system/appifi-bootstrap-update.service <<EOF
+[Unit]
+Description=Appifi Bootstrap Update
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/node /wisnuc/bootstrap/appifi-bootstrap-update.packed.js
+EOF
+
+cat > /lib/systemd/system/appifi-bootstrap-update.timer <<EOF
+[Unit]
+Description=Runs Appifi Bootstrap Update every 4 hour
+
+[Timer]
+OnBootSec=5min
+OnUnitActiveSec=4h
+Unit=appifi-bootstrap-update.service
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable appifi-bootstrap
+systemctl enable appifi-bootstrap-update.timer
+
+
---
+++ install-scripts/ubuntu-16-04-3-amd64/mkiso.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+# generated iso file name
+IMAGE=ubuntu-16.04.2-server-amd64-wisnuc.iso
+# original iso mount point
+ISO=iso
+# working directory
+BUILD=cd-image
+
+# check path argument
+if [ ! -f "$1" ]; then
+  echo "please provide iso image file path"
+  exit 1
+fi
+
+# clean
+sudo rm -rf $ISO
+sudo rm -rf $BUILD
+
+# mount iso
+mkdir -p $ISO
+sudo mount -o loop $1 $ISO
+
+# cp all iso files into build folder
+mkdir -p $BUILD
+cp -rT $ISO $BUILD
+sudo umount $ISO
+
+# fix permission
+chmod a+w $BUILD/preseed/ubuntu-server.seed
+
+cat <<'EOF' >> $BUILD/preseed/ubuntu-server.seed
+# Install wisnuc installer
+d-i preseed/late_command string \
+in-target wget -O /tmp/preseed-install https://raw.githubusercontent.com/wisnuc/appifi-system/master/install-scripts/ubuntu-16-04-2-amd64/preseed-install; \
+in-target bash -c "bash -x /tmp/preseed-install > /preseed-install.log 2>&1"
+EOF
+
+chmod a+w $BUILD/isolinux/isolinux.bin
+mkisofs -r -V "ubuntu-server 16.04.2 wisnuc" \
+  -cache-inodes \
+  -J -l -b isolinux/isolinux.bin \
+  -c isolinux/boot.cat -no-emul-boot \
+  -boot-load-size 4 -boot-info-table \
+  -o $IMAGE $BUILD
+
+rm -rf $ISO
+sudo rm -rf $BUILD
+
